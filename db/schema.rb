a353084261b500a1a36e94899b1c8333037c8118
rabebe <ruthie.abebe@gmail.com>
2025-10-27 18:07:52 -0400

    DB: Add unique and non-nullable constraints to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_18_204643) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_27_220207) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,5 +21,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_18_204643) do
     t.datetime "published_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["username"], name: "index_users_on_username"
   end
 end
