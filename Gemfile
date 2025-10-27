95774525051141ccfb787d1a2014f4c1f6789ae9
rabebe <ruthie.abebe@gmail.com>
2025-10-27 18:25:08 -0400

    Feat: Add User model with secure password and basic validations

--- Gemfile
+++ Gemfile
@@ -47,4 +47,7 @@ group :development, :test do
 
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
   gem "rubocop-rails-omakase", require: false
+
+  # Use brcypt for hashing passwords
+  gem "bcrypt", "~> 3.1.20"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+  # --- Security: Password Hashing ---
+  has_secure_password
+
+  # --- Validations ---
+
+  # Ensure presence and uniqueness of username
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false }
+            length; { minimum: 3, maximum: 25 }
+  # Ensure presence and uniqueness of email
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+  # Ensure password is present and long enough when creating a new user
+  validates :password,
+            presence: true,
+            length: { minimum: 6 },
+            on: :create
 end
