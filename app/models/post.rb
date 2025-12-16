5486b60bf39711390fc469d3d073c799ed5c9546
rabebe <ruthie.abebe@gmail.com>
2025-12-15 21:36:01 -0500

    Feat: Add comments and likes features

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
-  # Validations for robustness (ensures frontend sends data)
+  # Validations for Post attributes
   validates :title, presence: true, length: { minimum: 5, maximum: 100 }
   validates :body, presence: true
+
+  # Access author
+  def author_name
+    user.username
+  end
 end
