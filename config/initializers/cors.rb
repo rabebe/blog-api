6611a7a2fb11e7b043151799786fd9938c968f34
rabebe <ruthie.abebe@gmail.com>
2025-12-15 21:51:15 -0500

    Config: Allow blog frontend domain via CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://ruthabebe.com"
-
+    origins "http://localhost:3002", "https://ruthabebe.com"
     resource "*",
       headers: :any,
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
