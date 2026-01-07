d5dfec607983b6462ff486f472f19121e238b18a
rabebe <ruthie.abebe@gmail.com>
2026-01-07 14:00:06 -0500

    Add frontend domains to CORS config and expose Authorization header

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3002", "https://ruthabebe.com", "https://blog-frontend-jd7j.vercel.app/"
+    origins "http://localhost:3002", "https://ruthabebe.com", "https://blog-frontend-jd7j.vercel.app"
     resource "*",
       headers: :any,
+      expose: [ "Authorization" ],
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
   end
 end
