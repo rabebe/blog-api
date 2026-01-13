cf7a697a514ec6fd78de8b439af4e7b5f345fefe
rabebe <ruthie.abebe@gmail.com>
2026-01-12 22:03:53 -0500

    Configure Action Mailer in production with frontend host and HTTPS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,10 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: "https://blog-frontend-jd7j.vercel.app" }
+  config.action_mailer.default_url_options = {
+    host: "blog-frontend-jd7j.vercel.app",
+    protocol: "https"
+  }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
